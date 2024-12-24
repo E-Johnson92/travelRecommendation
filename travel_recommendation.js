f6cff00d3fd2b1984259daadce1103f7f007b0c9
e-johnson92 <johnson.evan92@gmail.com>
2024-12-24 15:11:02 -0500

    began adding javascript

---
+++ travel_recommendation.js
@@ -0,0 +1,7 @@
+const searchBtn = document.getElementById("search-btn");
+const searchInp = document.getElementsById("search").value; 
+const clearBtn = document.getElementById("reset-btn");  
+
+function searchLocation(searchInp) {
+    let filtered = tolowercase(searchInp).trim();  
+}
