1de0e388ae87e88ab3faec82cd93f8f64f8e7f81
e-johnson92 <johnson.evan92@gmail.com>
2024-12-27 18:55:54 -0500

    quick update

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -3,5 +3,14 @@ const searchInp = document.getElementsById("search").value;
 const clearBtn = document.getElementById("reset-btn");  
 
 function searchLocation(searchInp) {
-    let filtered = tolowercase(searchInp).trim();  
+    let filtered = searchInp.toLowerCase().trim();  
+    const resultDiv; 
+
+    fetch("./travel_recommendation.json")
+        .then(response => response.json)
+        .then(data =>{
+            const destination = data.destination.find(place => place.name.toLowerCase() === searchInp); 
+            
+
+        })
 }
