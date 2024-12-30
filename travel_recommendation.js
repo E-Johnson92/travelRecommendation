44079b2ef2ca8815c572db5b9a44b441e3af4fd6
E-Johnson92 <johnson.evan92@gmail.com>
2024-12-29 19:38:18 -0500

    Update travel_recommendation.js

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -11,23 +11,27 @@ function searchLocation() {
     const resultDiv = document.getElementById("results");
 
     fetch("./travel_recommendation_api.json")
-        .then(response => response.json()) 
+        .then(response => {
+            if(!response.ok){
+                throw new Error("Status Error: " + response.statusText); 
+            }
+            return response.json()
+        })
         .then(data => {
-       
-         const location = data.find(item => item.toLowerCase() === searchInp);
+            const location = data.find(item => item.name.toLowerCase() === searchInp);
 
-         if(location){
-            resultDiv.innerHTML = `<img src="${data.imageURL}"></img><br>
-                                    <h1>${data.name}</h1>
-                                    <p>${data.description}</p>` 
-         }
-         else
-         {
-            resultDiv.innerHTML = "Could not find data"; 
-         }
-            
-        }); 
-       
+            if (location) {
+                resultDiv.innerHTML = `<img src="${location.imageURL}" alt="${location.name}"><br>
+                                        <h1>${location.name}</h1>
+                                        <p>${location.description}</p>`;
+            } else {
+                resultDiv.innerHTML = "Could not find data";
+            }
+        })
+        .catch(error => {
+            resultDiv.innerHTML = "An error occurred while fetching data";
+            console.error('Error fetching data:', error);
+        });
 }
 
 searchBtn.addEventListener("click", searchLocation);
